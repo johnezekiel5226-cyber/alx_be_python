bdaf7448c247dfd12aad416346623ca1e1ccf9ce
Ezekiel john <johnezekiel526@gmail.com>
2025-09-09 10:50:26 -0700

    Python course

---
+++ ezekiel.py
@@ -0,0 +1,11 @@
+message = "Hello world"
+print(message)
+
+
+
+
+
+
+
+
+
---
+++ future_age_calculator.py
@@ -0,0 +1,6 @@
+current_age = int(input("how old are you: "))
+current_year = 2023
+future_year = 2050
+difference = 27
+future_age = current_age + difference
+print("In 2050", "you will be", future_age, "years old")
